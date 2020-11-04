e0cc8a0c5a7008df3b63b9ef9f9869c6b26ad988
ToasterBox834 <73861713+ToasterBox834@users.noreply.github.com>
2020-11-04 13:30:08 +1100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,7 @@
-@echo off
+@echo on
 
+echo Welcome!
+pause
+echo We will now create a folder for this script. It will not harm you pc in any way because the command mkdir only makes the folder.
 REM Script Name
-rmdir Scripts(PutFileIn)
+mkdir Scripts(PutFileIn)
