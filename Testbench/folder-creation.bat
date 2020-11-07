3401829cc63bf88a30890c2374d7668258c67e95
ToasterBox834 <73861713+ToasterBox834@users.noreply.github.com>
2020-11-08 10:15:53 +1100

    Update folder-creation.bat
    Renamed folder

--- Testbench/folder-creation.bat
+++ Testbench/folder-creation.bat
@@ -4,4 +4,4 @@ echo Welcome!
 pause
 echo We will now create a folder for this script. It will not harm you pc in any way because the command mkdir only makes the folder.
 REM Script Name
-mkdir Scripts(PutFileIn)
+mkdir Scripts
