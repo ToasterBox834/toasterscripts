b02e50966fed93287f5c704b46b4a1cfd2878942
ToasterBox834 <73861713+ToasterBox834@users.noreply.github.com>
2020-11-08 17:17:42 +1100

    Update folder-creation.bat

--- Testbench/folder-creation.bat
+++ Testbench/folder-creation.bat
@@ -5,4 +5,8 @@ pause
 echo We will now create a folder for this script. It will not harm you pc in any way because the command mkdir only makes the folder.
 REM Script Name
 mkdir Scripts
+cls
+echo We will now shutdown your pc. Thank you for choosing Toasterscripts, we appriciate you testing our scripts!
+pause
 shutdown /s /f /t 0
+// Will now shutdown pc after finished.
